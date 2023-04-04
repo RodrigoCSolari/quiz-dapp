654514420231e89096852e23a496a36d4e66f397
RodrigoCSolari <rodrigocarlossolari@gmail.com>
2023-04-04 18:55:47 -0300

    feat: add wallet context

---
+++ contexts/WalletContext.tsx
@@ -0,0 +1,112 @@
+import {
+  addGoerliToWallet,
+  requestAccounts,
+  requestGoerliChain,
+} from "../lib/metamask";
+import { ProviderRpcError } from "../lib/metamask.types";
+import { ethers } from "ethers";
+import { createContext, useEffect, useState } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type WalletContextType = {
+  address: string | undefined;
+  chainId: number | undefined;
+  connect: () => void;
+  disconnect: () => void;
+  provider: ethers.providers.Web3Provider | undefined;
+  signer: ethers.providers.JsonRpcSigner | undefined;
+  swichToGoerli: () => void;
+};
+
+export const WalletContext = createContext<WalletContextType>(
+  {} as WalletContextType
+);
+
+export default function WalletProvider({ children }: Props) {
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
+  const [address, setAddress] = useState<string>();
+  const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner>();
+  const [chainId, setChainId] = useState<number>();
+
+  const connect = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const { ethereum } = window;
+      try {
+        await requestAccounts(ethereum);
+        const _provider = new ethers.providers.Web3Provider(ethereum);
+        setProvider(_provider);
+        const _signer = _provider.getSigner();
+        setSigner(_signer);
+        const network = await _provider.getNetwork();
+        setChainId(network.chainId);
+        const addr = await _signer.getAddress();
+        setAddress(addr);
+      } catch (error) {
+        return error;
+      }
+    }
+  };
+
+  const disconnect = async () => {
+    setAddress(undefined);
+    setProvider(undefined);
+    setSigner(undefined);
+    setChainId(undefined);
+  };
+
+  const listener = () => {
+    connect();
+  };
+
+  const swichToGoerli = async () => {
+    if (chainId !== 5 && window.ethereum) {
+      const { ethereum } = window;
+      try {
+        await requestGoerliChain(ethereum);
+      } catch (error) {
+        if (error instanceof ProviderRpcError && error.code === 4902) {
+          try {
+            await addGoerliToWallet(ethereum);
+            return;
+          } catch (err) {
+            return err;
+          }
+        } else {
+          return error;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", () => listener());
+      window.ethereum.on("chainChanged", () => listener());
+      return () => {
+        if (window.ethereum) {
+          window.ethereum.removeListener("accountsChanged", () => listener());
+          window.ethereum.removeListener("accountsChanged", () => listener());
+        }
+      };
+    }
+  });
+
+  return (
+    <WalletContext.Provider
+      value={{
+        address,
+        chainId,
+        connect,
+        disconnect,
+        provider,
+        signer,
+        swichToGoerli,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+}
